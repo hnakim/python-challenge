8d8a158f30035c4bb69b5e9e6dcf31fda692fe45
Hannah Kim <hyoyun.k@gmail.com>
2022-10-02 20:03:54 -0500

    updated main.py to export to txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -34,31 +34,32 @@ total_votes = sum(voter_counts.values())
 #find winning count
 winning_vote = max(voter_counts.values())
 winner = max(voter_counts, key=voter_counts.get)
+winner_name =str(winner) + "\n"
 
 #print analysis to terminal and to txt file
 output_file = os.path.join("analysis", "analysis.txt")
 with open(output_file, "w") as txt_file:
 
-    print("Election Results")
-    print("----------------------------")
-    print(f"Total Votes: {total_votes}")
-    print("----------------------------")
+    txt_file.write("Election Results\n")
+    txt_file.write("----------------------------\n")
+    txt_file.write(f"Total Votes: {total_votes}\n")
+    txt_file.write("----------------------------\n")
 
-#loop through dictionary to get vote count
-for candidate_name in voter_counts:
-    votes = voter_counts[candidate_name]
-    
-    #calculate percentage
-    percentage = (votes / (total_votes)*100)
-    percentage_rounded = round(percentage, 3)
+    #loop through dictionary to get vote count
+    for candidate_name in voter_counts:
+        votes = voter_counts[candidate_name]
+        
+        #calculate percentage
+        percentage = (votes / (total_votes)*100)
+        percentage_rounded = round(percentage, 3)
 
-    #candidate and vote summary
-    summary = f"{candidate_name}: {percentage_rounded}% ({votes})"
+        #candidate and vote summary
+        summary = f"{candidate_name}: {percentage_rounded}% ({votes})\n"
 
-    print(summary)
+        txt_file.write(summary)
 
-print("----------------------------")
-print(winner)
-print("----------------------------")
+    txt_file.write("----------------------------\n")
+    txt_file.write(winner_name)
+    txt_file.write("----------------------------")
 
-txt_file.write
+
